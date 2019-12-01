eab1de74ea676184bc318cc1010cb2167dfa135d
Stefan Bethke <stb@lassitu.de>
2019-12-01 14:10:09 +0100

    git secret?

--- gen-certs.sh
+++ gen-certs.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 
-for i in adams borg clarke dijkstra eliza; do
-  for j in 0 1 2; do
-    CN="$i-$j"
-    cat >.openssl.cnf <<EOF
+gencert() {
+  CN="$1"
+  cat >.openssl.cnf <<EOF
 [ req ]
 prompt = no
 distinguished_name = req_distinguished_name
@@ -13,15 +12,22 @@ O = c3lingo
 CN = ${CN}
 emailAddress = info@c3lingo.org
 EOF
-    [ ! -f certs/${CN}-key.pem ] && \
-    openssl req -x509 -newkey rsa:4096 -days 365 -nodes -config .openssl.cnf \
-      -keyout certs/${CN}-key.pem \
-      -out certs/${CN}-cert.pem
-    openssl pkcs12 -export -passout pass: \
-      -out certs/${CN}.p12 \
-      -inkey certs/${CN}-key.pem \
-      -in certs/${CN}-cert.pem
+  [ ! -f certs/${CN}-key.pem ] && \
+  openssl req -x509 -newkey rsa:4096 -days 365 -nodes -config .openssl.cnf \
+    -keyout certs/${CN}-key.pem \
+    -out certs/${CN}-cert.pem
+  openssl pkcs12 -export -passout pass: \
+    -out certs/${CN}.p12 \
+    -inkey certs/${CN}-key.pem \
+    -in certs/${CN}-cert.pem
+}
+
+for i in adams borg clarke dijkstra eliza; do
+  for j in 0 1 2; do
+    gencert "$i-$j"
   done
 done
+
+gencert test
 
 rm .openssl.cnf
